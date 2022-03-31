a4cb50fc5344e5af5bd7b9736ed390dc8f2326c7
Yayawak <elio.interesante@gmail.com>
2022-04-01 02:09:56 +0700

    successly fetch commerce.js api and display lists of products to page

--- e-comm-proj/src/components/Products/Product/Product.jsx
+++ e-comm-proj/src/components/Products/Product/Product.jsx
@@ -5,22 +5,23 @@ import { AddShoppingCart } from '@material-ui/icons'
 import useStyles from './styles'
 const Product = ({ product }) => {
     const classes = useStyles();
+    console.log(product);
     return (
         <div>
             <Card className={classes.root}>
-                <CardMedia className={classes.media} image='' title={product.name} />
+                <CardMedia className={classes.media} image={product.image.url} title={product.name} />
                 <CardContent>
                     <div className={ classes.cardContent }>
                         <Typography variant='h5' gutterBottom>
                             {product.name}
                         </Typography>
                         <Typography variant='h5' >
-                            {product.price}
-                        </Typography>
-                        <Typography variant='body2' color='textSecondary' >
-                            {product.description}
+                            {product.price.formatted_with_symbol}
                         </Typography>
                     </div>
+                        <Typography 
+                            dangerouslySetInnerHTML={{__html: product.description}} variant='body2' color='textSecondary' 
+                        />
                 </CardContent>
                 <CardActions className={classes.cardActions} disableSpacing>
                     <IconButton aria-label='Add to Cart'>
--- e-comm-proj/src/components/Products/Products.jsx
+++ e-comm-proj/src/components/Products/Products.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Grid } from '@material-ui/core'
 import Product from "./Product/Product";
 import useStyles from './styles'
-const products = [
-    { id: 1, name: 'Shoes', description: 'Runnig shoes.', price:'$5'}
-    ,{id:2, name: 'Macbook', description: 'Apple macbook.', price:'$10'}
-]
-const Products = () => {
+const Products = ({products}) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
